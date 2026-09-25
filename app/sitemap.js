5876e7273ba8d51797dde74691775060f10a58f6
veravoxlocalization-create <veravoxlocalization@gmail.com>
2026-09-25 19:12:38 -0300

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,18 @@
+export default function sitemap() {
+  const baseUrl = 'https://front-lac-theta.vercel.app/'; 
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
+}
